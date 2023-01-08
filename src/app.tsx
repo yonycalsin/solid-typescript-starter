f5d5651b192d255cee1119be9864556ac1420b70
Yony Calsin <helloyonycalsin@gmail.com>
2023-01-08 17:18:43 -0500

    feat: add solidjs

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1>Solid + Typescript - Starter</h1>
+      <p>
+        <i>
+          Created By <a href="https://www.yonycalsin.com/">Yony Calsin</a>
+        </i>
+      </p>
+    </div>
+  )
+}
+
+export default App
